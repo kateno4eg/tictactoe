259ce2deb756f9c1116f39a7b8c66d441ca78074
kateno4eg <82028605+kateno4eg@users.noreply.github.com>
2021-04-06 17:23:17 +0300

    Commit 1.1

--- scripts.py
+++ scripts.py
@@ -3,8 +3,7 @@
 grid = [["-" for col in range(3)] for row in range(3)]   # создали пустую сетку
 print(*grid, sep='\n')  # вывели ее некрасиво
 turn = input("Крестики ходят: ")  # формат записи хода "column-row", например 2-2
-value = list(turn)
-value.remove(" ")
+value = list(map(int, turn.split(' ')))
 print(value)
 # grid.insert()
 # print(*grid, sep='\n')
