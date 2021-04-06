5e90f21ef3e185f0c2adbff3abaec80235436220
kateno4eg <82028605+kateno4eg@users.noreply.github.com>
2021-04-06 17:17:47 +0300

    Commit 1

--- scripts.py
+++ scripts.py
@@ -1,1 +1,10 @@
-print("Hello,World!")
+# print("Hello,World!")
+
+grid = [["-" for col in range(3)] for row in range(3)]   # создали пустую сетку
+print(*grid, sep='\n')  # вывели ее некрасиво
+turn = input("Крестики ходят: ")  # формат записи хода "column-row", например 2-2
+value = list(turn)
+value.remove(" ")
+print(value)
+# grid.insert()
+# print(*grid, sep='\n')
