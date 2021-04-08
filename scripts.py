13df9329a8abbbc6df7cf79159775a903176b305
kateno4eg <82028605+kateno4eg@users.noreply.github.com>
2021-04-08 17:34:55 +0300

    Commit 1.5

--- scripts.py
+++ scripts.py
@@ -1,54 +1,61 @@
 
 n = 3
-m = 3
-turns = n * m  # количество возможных ходов
-grid = [["-" for col in range(n)] for row in range(m)]   # создали пустую сетку
-print(*grid, sep='\n')  # вывели сетку
+grid = [["-" for col in range(n)] for row in range(n)]
+sign = 'X'
+move = input("Крестики ходят первые: ")
+col, row = list(map(int, move.split(' ')))
+grid[col - 1][row - 1] = sign
+print(*grid, sep='\n')
 
 
-def free_seat(col, row):
+def update_grid(col, row, sign):
     if grid[col - 1][row - 1] == "-":
-        if turn % 2 == 0:
-            grid[col - 1][row - 1] = 'O'
-        else:
-            grid[col - 1][row - 1] = 'X'
-        return print(*grid, sep='\n')
+        grid[col - 1][row - 1] = sign
+        return new_grid(grid)
     else:
-        return input("Место занято, сходите иначе: ")
+        return input('Место занято, сходите иначе: '), update_grid(col, row, sign)
 
 
-for turn in range(1, turns + 1):
-    if turn == 1:
-        turn = input("Крестики ходят первые: ")  # формат хода "row-column", например 1 2, где 1 - строка, 2 -столбец
-        col, row = list(map(int, turn.split(' ')))
-        if grid[col - 1][row - 1] == "-":
-            grid[col - 1][row - 1] = 'X'
-        else:
-            turn = input("Место занято, сходите иначе: ")
-        print(*grid, sep='\n')
-    elif turn % 2 == 0:
-        turn = input("Нолики ходят: ")
-        col, row = list(map(int, turn.split(' ')))
-        if grid[col - 1][row - 1] == "-":
-            grid[col - 1][row - 1] = 'O'
-        else:
-            turn = input("Место занято, сходите иначе: ")
-        print(*grid, sep='\n')
+def new_grid(a):
+    print(*a, sep='\n')
+
+
+while "-" in grid:
+    if sign == "X":
+        sign = "O"
+        move = input("Нолики ходят: ")
+        grid = update_grid(col, row, sign)
     else:
-        turn = input("Крестики ходят: ")
-        col, row = list(map(int, turn.split(' ')))
-        if grid[col - 1][row - 1] == "-":
-            grid[col - 1][row - 1] = 'X'
-        else:
-            turn = input("Место занято, сходите иначе: ")
-        print(*grid, sep='\n')
+        sign = "X"
+        move = input("Крестики ходят: ")
+        grid = update_grid(col, row, sign)
 
 
 
+#  def winner_is():
 
-
-
-
-
-
-
+#  for turn in range(1, turns + 1):
+#     if turn == 1:
+#         turn = input("Крестики ходят первые: ")  # формат хода "row-column", например 1 2, где 1 - строка, 2 -столбец
+#         col, row = list(map(int, turn.split(' ')))
+#         if grid[col - 1][row - 1] == "-":
+#             grid[col - 1][row - 1] = 'X'
+#         else:
+#             turn = input("Место занято, сходите иначе: ")
+#         print(*grid, sep='\n')
+#     elif turn % 2 == 0:
+#         turn = input("Нолики ходят: ")
+#         col, row = list(map(int, turn.split(' ')))
+#         if grid[col - 1][row - 1] == "-":
+#             grid[col - 1][row - 1] = 'O'
+#         else:
+#             turn = input("Место занято, сходите иначе: ")
+#         print(*grid, sep='\n')
+#     else:
+#         turn = input("Крестики ходят: ")
+#         col, row = list(map(int, turn.split(' ')))
+#         if grid[col - 1][row - 1] == "-":
+#             grid[col - 1][row - 1] = 'X'
+#         else:
+#             turn = input("Место занято, сходите иначе: ")
+#         print(*grid, sep='\n')
