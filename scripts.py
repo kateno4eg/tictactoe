7c2baa64136ce6c65e43e08f42b57e38c7b0ae37
kateno4eg <kateno4eg@bk.ru>
2021-04-17 17:11:01 +0300

    Commit 2.0

--- scripts.py
+++ scripts.py
@@ -1,10 +1,6 @@
 def update_grid(column, row_, symbol):
-    while grid[column - 1][row_ - 1] == "-":
-        grid[column - 1][row_ - 1] = symbol
-        return new_grid(grid)
-    else:
-        input("Место занято, сходите иначе: ")
-#        return update_grid(column, row_, symbol)
+    grid[column - 1][row_ - 1] = symbol
+    return new_grid(grid)
 
 
 def new_grid(a):
@@ -14,25 +10,28 @@ def new_grid(a):
         print(i, ' '.join(map(str, item)))
 
 
-def winner_is(a, r):  # доработать
-    a_set = set(a[r])
-    if len(a_set) == 1 and a_set != {'-'}:
-        print(f'Победа {a_set}!')
-    else:
-        pass
+def winner_is(a):  # доработать
+    for el in a:
+        a_set = set(el)
+        if len(a_set) == 1 and a_set != {'-'}:
+            print(f'Победа {a_set}!')  # доработать вывод
+            return True
+        else:
+            pass
 
 
 n = 3
 grid = [["-" for c in range(n)] for r in range(n)]
 sign = ["X", "O"]
 next_sign = sign[0]
-move = input("Крестики ходят первые: ")  # включить в цикл?
+
+move = input("Крестики ходят первые: ")
 col, row = list(map(int, move.split(' ')))
 grid[col - 1][row - 1] = next_sign
 new_grid(grid)
 
-condition = any(['-' in row for row in grid])
-while condition:
+condition_1 = any(['-' in row for row in grid])
+while condition_1 and winner_is(grid) is not True:
     if next_sign == sign[0]:
         next_sign = sign[1]
         move = input("Нолики ходят: ")
@@ -40,10 +39,20 @@ while condition:
         next_sign = sign[0]
         move = input("Крестики ходят: ")
     col, row = list(map(int, move.split(' ')))
-    update_grid(col, row, next_sign)
+    if 0 < col <= n and 0 < row <= n:
+        if grid[col - 1][row - 1] == "-":
+            update_grid(col, row, next_sign)
+        else:
+            move = input("Место занято, сходите иначе: ")
+            col, row = list(map(int, move.split(' ')))
+            update_grid(col, row, next_sign)
+    else:
+        move = input(f"Введите число в диапазоне от 0 до {n} включительно: ")
+        col, row = list(map(int, move.split(' ')))
+        update_grid(col, row, next_sign)
     condition = any(['-' in row for row in grid])
-#    winner_is(grid, row)
+    winner_is(grid)
 else:
-    print("Игра закончилась вничью")
+    print("Игра окончена")  # добавить условие победы в игре
 
 
